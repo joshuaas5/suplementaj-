70ee9c90f870a255a8cccf8e2ec2313cd0ba5bd4
joshuaas5 <joshuaas500@gmail.com>
2025-12-08 14:03:17 -0300

    fix: usar tags script manuais para verificacao do Google Analytics

--- components/analytics/GoogleAnalytics.tsx
+++ components/analytics/GoogleAnalytics.tsx
@@ -1,28 +1,20 @@
 'use client'
 
-import Script from 'next/script'
-
 export function GoogleAnalytics() {
-  // Usar o novo ID do Google Analytics para suplementaja.vercel.app
-  const gaId = process.env.NEXT_PUBLIC_GA_ID || 'G-HZ95NQC0C1'
+  const gaId = 'G-HZ95NQC0C1'
 
   return (
     <>
-      <Script
-        strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${gaId}`}
-      />
-      <Script
-        id="google-analytics"
-        strategy="afterInteractive"
+      {/* Google tag (gtag.js) */}
+      <script async src={`https://www.googletagmanager.com/gtag/js?id=${gaId}`}></script>
+      <script
         dangerouslySetInnerHTML={{
           __html: `
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', '${gaId}', {
-              page_path: window.location.pathname,
-            });
+
+            gtag('config', '${gaId}');
           `,
         }}
       />
